b3b3196effcec18ea60346919e814cc1876491ab
Cphayim <admin@cphayim.me>
2018-06-03 23:02:18 +0800

    test: 数组测试用例

---
+++ src/com/cpahyim/test/ArrayTest.java
@@ -0,0 +1,40 @@
+package com.cpahyim.test;
+
+import com.cphayim.ds.Array;
+
+/**
+ * @author Cphayim
+ * @date Created in 2018/6/3 20:13
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+
+        Array<Integer> arr = new Array<>();
+        for (int i = 0; i < 10; i++)
+            arr.addLast(i);
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
+
+        Array<Student> students = new Array<>();
+        students.addLast(new Student("Cphayim", 85));
+        students.addLast(new Student("Hoyoe", 100));
+        students.addLast(new Student("Mammon", 59));
+
+        System.out.println(students);
+    }
+}
---
+++ src/com/cpahyim/test/Student.java
@@ -0,0 +1,21 @@
+package com.cpahyim.test;
+
+/**
+ * @author Cphayim
+ * @date Created in 2018/6/3 21:00
+ */
+public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student(name: %s, score: %d)", name, score);
+    }
+}
