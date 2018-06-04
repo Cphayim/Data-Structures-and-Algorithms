52985e24bc0bcfd8f01af6021b9d7d357bf9840d
Cphayim <admin@cphayim.me>
2018-06-04 14:04:18 +0800

    test: ArrayStack 测试

---
+++ src/com/cphayim/test/ArrayStackTest.java
@@ -0,0 +1,32 @@
+package com.cphayim.test;
+
+import com.cphayim.ds.ArrayStack;
+
+/**
+ * @author Cphayim
+ * @date Created in 2018/6/4 13:48
+ */
+public class ArrayStackTest {
+    public static void main(String[] args) {
+
+        ArrayStack<Integer> stack = new ArrayStack<>();
+
+        // 入栈测试
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        // 出栈测试
+        stack.pop();
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+
+        // 获取栈顶元素测试
+        System.out.println(stack.peek());
+
+        // 获取当前栈大小
+        System.out.println(stack.getSize());
+    }
+}
