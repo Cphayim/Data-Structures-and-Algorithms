4d64f0e8349ab7d421e54f410a18dc5aeb01de72
Cphayim <admin@cphayim.me>
2018-06-06 23:36:51 +0800

    test(ds): ArrayQueue 测试

---
+++ src/com/cphayim/test/ArrayQueueTest.java
@@ -0,0 +1,31 @@
+package com.cphayim.test;
+
+import com.cphayim.ds.ArrayQueue;
+
+/**
+ * @author Cphayim
+ * @date Created in 2018/6/6 23:23
+ */
+public class ArrayQueueTest {
+    public static void main(String[] args) {
+        ArrayQueue<Integer> quque = new ArrayQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            // 入队
+            quque.enqueue(i);
+            System.out.println(quque);
+
+            if (i % 3 == 2) {
+                // 出队
+                quque.dequeue();
+                System.out.println(quque);
+            }
+        }
+
+        // 获取当前队首元素
+        System.out.println(quque.getFront());
+
+        // 获取当前队列长度
+        System.out.println(quque.getSize());
+    }
+}
