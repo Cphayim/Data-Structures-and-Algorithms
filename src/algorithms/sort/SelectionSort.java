88f69315ed8f204175dfcdac326a14c775ee2364
Cphayim <i@cphayim.me>
2018-10-27 21:44:04 +0800

    optimize SelectionSort

--- src/algorithms/sort/SelectionSort.java
+++ src/algorithms/sort/SelectionSort.java
@@ -9,6 +9,7 @@ public class SelectionSort {
 
     public static void sort(Comparable[] arr) {
 
+        /*
         for (int i = 0; i < arr.length; i++) {
             // 寻找[i, n)区间里的最小值的索引
             int minIndex = i;
@@ -18,6 +19,31 @@ public class SelectionSort {
 
             // 交换最小值和索引为i的值的位置
             swap(arr, i, minIndex);
+        }
+        */
+
+        // 优化算法，双路查找
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int minIndex = left;
+            int maxIndex = right;
+
+            // 在每一轮查找时，要保证 arr[minIndex] <= arr[maxIndex]
+            if (arr[minIndex].compareTo(arr[maxIndex]) > 0)
+                swap(arr, minIndex, maxIndex);
+
+            for (int i = left + 1; i < right; i++) {
+                if (arr[i].compareTo(arr[minIndex]) < 0)
+                    minIndex = i;
+                else if (arr[i].compareTo(arr[maxIndex]) > 0)
+                    maxIndex = i;
+            }
+
+            swap(arr, left, minIndex);
+            swap(arr, right, maxIndex);
+
+            left++;
+            right--;
         }
     }
 
